cfcc164edb29df75a63ba3508d5715c6a8dfb430
PratikMate <pratikm470@gmail.com>
2022-07-20 23:50:43 +0530

    Download Page Completed all tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DownloadPage from './components/DownloadPage/DownloadPage';
 
 function App() {
   return (
     <div className="App">
-      hey
+      <DownloadPage />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
 );
