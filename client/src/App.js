261871d48a5b3be208df12ab21965173dc049765
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-21 10:43:42 +0530

    App Part

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import {Routes, Route} from "react-router-dom"
 import './App.css';
 import DownloadPage from './components/DownloadPage/DownloadPage';
+import FeaturePage from "./components/FeaturePage/FeaturePage";
 import LoginPage from './components/LoginPage/LoginPage';
 import SignupPage from './components/SignupPage/SignupPage';
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +13,7 @@ function App() {
         <Route path='/signup' element={<SignupPage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/download' element={<DownloadPage />} />
-        
+        <Route path='/features' element={<FeaturePage/>} />
       </Routes>
     </div>
   );
