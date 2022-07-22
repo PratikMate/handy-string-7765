42ab3e67609766894b57c9daefdddf509d3e14dd
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-22 12:53:30 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import FeaturePage from "./components/FeaturePage/FeaturePage";
 import LoginPage from './components/LoginPage/LoginPage';
 import SignupPage from './components/SignupPage/SignupPage';
 import Navbar from "./mainComponent/Navbar";
+import HomePage from "./components/HomePage/HomePage";
+import MainComponentApp from "./mainComponent/MainComponentApp";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Routes>
+        <Route path='/' element={<HomePage/>} />
         <Route path='/signup' element={<SignupPage/>} />
         <Route path='/login' element={<LoginPage/>} />
         <Route path='/download' element={<DownloadPage />} />
         <Route path='/features' element={<FeaturePage/>} />
+        <Route path='/maincomponent' element={<MainComponentApp/>} />
       </Routes>
     </div>
   );
---
+++ client/src/mainComponent/SubComponents/SideNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SideNav
