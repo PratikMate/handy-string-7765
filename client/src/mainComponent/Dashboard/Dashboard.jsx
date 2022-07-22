c7111f504f2d73100824e7e7b1e4fd8db068b694
piggybackcoder1 <swainrakeshkumar60@gmail.com>
2022-07-22 10:40:19 +0530

    changes dashbaord

--- client/src/mainComponent/Dashboard/Dashboard.jsx
+++ client/src/mainComponent/Dashboard/Dashboard.jsx
@@ -1,29 +1,34 @@
-import {Container,ListGroup,ListGroupItem,Dropdown,DropdownButton,} from "react-bootstrap";
+import {Container,ListGroup,ListGroupItem} from "react-bootstrap";
+import {Menu,MenuButton,MenuList,MenuItem} from '@chakra-ui/react';
 import DashboardImg from "./assets/Clockifydashboard.png";
 import "./styles/Dashboard.module.css";
 const Dashboard = () => {
   return (
-    <Container className='pagenav'>
-      <ListGroup style={{display: "flex",margin:"20px",marginTop:"40px",justifyContent: "spaceBetween",flexDirection:"row",gap:"850px"}}>
+    <Container className='pagenav' style={{backgroundColor:"#e4eaee"}}>
+      <ListGroup style={{display: "flex",justifyContent: "spaceBetween",flexDirection:"row",gap:"820px"}}>
         <ListGroup.Item
-          style={{ fontSize: "1.8rem", color: "#757575", border: "none" }}>
+          style={{ fontSize: "1.8rem", color: "#757575", border: "" ,marginTop:"10px",marginLeft:"2rem"}}>
           Dashboard
         </ListGroup.Item>
-        <ListGroupItem style={{display:"flex",flexDirection:"row", justifyContent: "spaceBetween",gap:"20px"}}>
-          <DropdownButton id='dropdown-basic-button' title='Project'>
-            <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
-            <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
-            <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
-          </DropdownButton>
-          <DropdownButton id='dropdown-basic-button' title='Only Me'>
-            <Dropdown.Item href='#/action-1'>Action</Dropdown.Item>
-            <Dropdown.Item href='#/action-2'>Another action</Dropdown.Item>
-            <Dropdown.Item href='#/action-3'>Something else</Dropdown.Item>
-          </DropdownButton>
+        <ListGroupItem style={{display:"flex",flexDirection:"row", justifyContent: "spaceBetween",gap:"20px" ,marginTop:"10px",padding:"10px"}}>
+        <Menu isLazy w="10%">
+          <MenuButton p="8px"  backgroundColor="white" color="black" marginTop="10px" >Projects</MenuButton>
+          <MenuList>
+          <MenuItem>Projects</MenuItem>
+            <MenuItem>Billability</MenuItem>
+          </MenuList>
+        </Menu>
+        <Menu isLazy w="10%">
+          <MenuButton p="8px"  backgroundColor="white" color="black" marginTop="10px" paddling="20px" >Only Me</MenuButton>
+          <MenuList>
+          <MenuItem>Only Me</MenuItem>
+            <MenuItem>Team</MenuItem>
+          </MenuList>
+        </Menu>
           <input
             className='Input'
             type='date'
-            style={{ color: "#757575", width: "15rem", height: "40px" }}
+            style={{ color: "#757575", width: "15rem", height: "40px",marginTop:"10px" }}
           />
         </ListGroupItem>
       </ListGroup>
