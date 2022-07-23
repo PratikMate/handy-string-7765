4ae469073b70056af4b444233b38a85007f1d2c3
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-23 18:18:50 +0530

    Few Parts Added and CSS Updated

--- client/src/components/HomePage/MidPart/Part9.jsx
+++ client/src/components/HomePage/MidPart/Part9.jsx
@@ -1,51 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-
 import { FaUsers } from "react-icons/fa";
 
-const Part9 = () => {
-  const Wrapper = styled.div`
-    width: 60vw;
-    margin: 2rem auto;
-    color: #546e7a;
-  `;
-  const Ul = styled.ul`
-    display: flex;
-    width: 30vw;
-    margin: auto;
-    justify-content: space-around;
-    text-align: center;
-  `;
-  const Button = styled.button`
-    background: #0288d1;
-    color: white;
-    padding: 15px 30px;
-    border-radius: 5px;
-    border: #0288d1;
-    font-weight: 600;
-    margin-top: 10px;
-    cursor: pointer;
+const Wrapper = styled.div`
+  padding:100px;
+  background: #f7fcff;
+  width: 100%;
+  margin: 2rem auto;
+  color: #546e7a;
+`;
+const Ul = styled.ul`
+  display: flex;
+  width: 30vw;
+  margin: auto;
+  justify-content: space-around;
+  text-align: center;
+`;
+const Button = styled.button`
+  background: #0288d1;
+  color: white;
+  padding: 15px 30px;
+  border-radius: 5px;
+  border: #0288d1;
+  font-weight: 600;
+  margin-top: 10px;
+  cursor: pointer;
 
-    :hover {
-      background: #046997;
-    }
-  `;
-  const UserNoSpan = styled.span`
-    font-size: 12px;
-    margin: 0px 5px;
-    cursor: pointer;
-  `;
-  const UserNoDiv = styled.div`
-    margin-top: 10px;
-    // border: 5px solid red;
-    justify-content: center;
-    display: flex;
-    align-items: center;
-    color: #0288d1;
-  `;
+  :hover {
+    background: #046997;
+  }
+`;
+const UserNoSpan = styled.span`
+  font-size: 12px;
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const UserNoDiv = styled.div`
+  margin-top: 10px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  color: #0288d1;
+`;
+const Part9 = () => {
   return (
     <Wrapper>
-      <h2>Start tracking time with Clockify</h2>
+      <p style={{fontSize:'34px', color:'black'}}>Start tracking time with Clockify</p>
       <Ul>
         <li>24/7 Support</li>
         <li>Cancel Anytime</li>
