40d264527ddd51053d7acd41f7e2410768fafaa2
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 03:43:19 +0530

    Main Component Completed

--- client/src/mainComponent/MainComponentApp.jsx
+++ client/src/mainComponent/MainComponentApp.jsx
@@ -2,6 +2,14 @@ import React,{useState} from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './Navbar'
 import SideNav from './NavbarAndSideBar/SideNav'
+import TimeTracker from './Time Tracker/TimeTracker'
+import Calendar from './SubComponents/Calendar'
+import Dashboard from './SubComponents/Dashboard'
+import Reports from './SubComponents/Reports'
+import Projects from './SubComponents/Projects'
+import Team from './SubComponents/Team'
+import Clients from './SubComponents/Clients'
+import Tags from './SubComponents/Tags'
 import Setting from './SubComponents/Setting'
 
 const MainComponentApp = () => {
@@ -17,12 +25,18 @@ const MainComponentApp = () => {
             <div  style={toggle ? {width:"218px"} : {width:'53px'}}>
               <SideNav props = {toggle}/>
             </div>
-            <div style={{background:'#f2f6f8', width:'100%', marginTop:'50px'}}>
+            <div style={{background:'#f2f6f8', width:'100%', marginTop:'50px',height:'100vh'}}>
               <Routes>
-                <Route path='/setting' element={<Setting/>} />
+                <Route path='/timetracker' element={<TimeTracker/>} />
+                <Route path='/calendar' element={<Calendar/>} />
+                <Route path='/dashboard' element={<Dashboard/>} />
+                <Route path='/reports' element={<Reports/>} />
+                <Route path='/projects' element={<Projects/>} />
+                <Route path='/team' element={<Team/>} />
+                <Route path='/clients' element={<TimeTracker/>} />
+                <Route path='/tags' element={<Tags/>} />
                 <Route path='/setting' element={<Setting/>} />
               </Routes>
-              {/* <Setting/> */}
             </div>
         </div>
     </div>
