7076af3600520f307634e2d33dbd46633296790e
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 03:44:01 +0530

    SubComponent Calendar Added

---
+++ client/src/mainComponent/SubComponents/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from '../../assets/img/MainComponentImages/calendar.png'
+
+const Calendar = () => {
+  return (
+    <div>
+      <img src={img} alt='' />
+    </div>
+  )
+}
+
+export default Calendar
