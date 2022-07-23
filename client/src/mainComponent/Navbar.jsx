ef5c2e3a0e3a147b30dd94187e1e17440726f18b
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-23 18:19:27 +0530

    CSS and media query Updated

--- client/src/mainComponent/Navbar.jsx
+++ client/src/mainComponent/Navbar.jsx
@@ -6,6 +6,7 @@ import quemark from "../../src/assets/svg/quemark.svg"
 import dropdown from "../../src/assets/svg/Dropdown.png"
 import { Box, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
 import styled from "styled-components";
+import { Link } from 'react-router-dom'
 
 const Div1 = styled.div`
 width: 100%;
@@ -15,17 +16,19 @@ justify-content: space-between;
 box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `
 const Div2 = styled.div`
-width: 60%;
+width: fit-content;
 display: flex;
 gap: 20px;
 margin-left: 18px;
 align-items: center;
+cursor:pointer;
 `
 const Div3 = styled.div`
-width: 32%;
-display: grid;
-grid-template-columns: 45% 18% 10% 10% 10%;
+width: fit-content;
+display: flex;
+gap:15px;
 align-items: center;
+padding-right:10px;
 `
 const Button1 = styled.button`
     padding: 4px 8px;
@@ -44,19 +47,19 @@ const Button1 = styled.button`
 `
 
 const Navbar = (props) => {
-  
+
   const handleToggle = () => {
     props.props.toggle ? props.props.toggleNavbar(false) : props.props.toggleNavbar(true)
   }
   return (
-      <Div1>
-          <Div2>
-              <img src={options} alt="error" onClick={() => handleToggle()} width="27px" height="27px"/>
-              <img src={logo} alt="error" width="93px" height="24px"/>
-          </Div2>
-          <Div3>
+    <Div1>
+      <Div2>
+        <img src={options} alt="error" onClick={() => handleToggle()} width="27px" height="27px" />
+        <Link to='/'><img src={logo} alt="error" width="93px" height="24px" /></Link>
+      </Div2>
+      <Div3>
         <Menu isLazy>
-          
+
           <MenuButton><Box display="flex"> <Text>Pratik Mate's Workspace</Text><img src={dropdown} alt="error" width="30px" /></Box></MenuButton>
           <MenuList>
             {/* MenuItems are not rendered unless Menu is open */}
@@ -74,7 +77,7 @@ const Navbar = (props) => {
           <img src={notification} alt="error" width="16px" height="20px" />
         </Box>
         <Menu isLazy w="10%">
-          <MenuButton p="8px 0" borderRadius="50%" backgroundColor="#03a9f4" color="white"  >PM</MenuButton>
+          <MenuButton p="8px" borderRadius="50%" backgroundColor="#03a9f4" color="white"  >PM</MenuButton>
           <MenuList>
             <MenuItem>Pratik mate</MenuItem>
             <MenuItem>Profile settings</MenuItem>
@@ -83,7 +86,7 @@ const Navbar = (props) => {
             <MenuItem>Try chat app</MenuItem>
           </MenuList>
         </Menu>
-          </Div3>
+      </Div3>
     </Div1>
   )
 }
