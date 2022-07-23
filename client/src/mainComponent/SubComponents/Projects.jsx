811300129d9596cb07c41ed4b0f82614463651fa
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 03:44:43 +0530

    SubComponent Projects Added

---
+++ client/src/mainComponent/SubComponents/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from '../../assets/img/MainComponentImages/calendar.png'
+
+const Projects = () => {
+  return (
+    <div>
+      <img src={img} alt='' />
+    </div>
+  )
+}
+
+export default Projects
