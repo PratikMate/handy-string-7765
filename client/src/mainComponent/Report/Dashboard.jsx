ba753482b63f90e886082fb2e6c3f718ddd43464
piggybackcoder1 <swainrakeshkumar60@gmail.com>
2022-07-23 10:36:18 +0530

    two components added

---
+++ client/src/mainComponent/Report/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <img
+        alt='card'
+        style={{ marginRight: "2rem", width: "100%",marginLeft:"2rem" }}
+        src="https://miro.medium.com/max/1400/1*gUsaWfxrWaPSJA2lAYSUjw.png"></img> 
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ client/src/mainComponent/Report/Setting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Setting = () => {
+  return (
+    <div>
+         <img
+        alt='card'
+        style={{ marginRight: "2rem", width: "100%",marginLeft:"2rem" }}
+        src="https://miro.medium.com/max/1400/1*P0bYYGBphKc_Q5Ho55CfDg.png"></img> 
+    </div>
+  )
+}
+
+export default Setting
