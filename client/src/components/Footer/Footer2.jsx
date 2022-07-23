6e8801bb3d0b8b3376b10f6bfe3c5b927d50d4bc
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-23 18:16:07 +0530

    Footer2 Updated

--- client/src/components/Footer/Footer2.jsx
+++ client/src/components/Footer/Footer2.jsx
@@ -8,34 +8,33 @@ import {
 } from "react-icons/ai";
 import styled from "styled-components";
 
-const Footer2 = () => {
-  const SpanElem = styled.span`
-    width: 200px;
-    font-size: 24px;
-    padding: 0 8px;
-    // border: 3px solid red;
-    :hover {
-      cursor: pointer;
-      color: #03a9f4;
-    }
-  `;
+const SpanElem = styled.span`
+  width: fit-content;
+  font-size: 20px;
+  padding: 0 4px;
+  :hover {
+    cursor: pointer;
+    color: #03a9f4;
+  }
+`;
 
-  const P = styled.p`
-    font-size: 12px;
-    :hover {
-      cursor: pointer;
-      color: #03a9f4;
-    }
-  `;
-  const Span = styled.span`
-    margin: 0 7px;
-    // border: 1px solid black;
-    font: 14px;
-    :hover {
-      cursor: pointer;
-      color: #03a9f4;
-    }
-  `;
+const P = styled.p`
+  font-size: 12px;
+  padding:15px 0px 0px 10px;
+  :hover {
+    cursor: pointer;
+    color: #03a9f4;
+  }
+`;
+const Span = styled.span`
+  margin: 0 7px;
+  font-size: 14px;
+  :hover {
+    cursor: pointer;
+    color: #03a9f4;
+  }
+`;
+const Footer2 = () => {
   return (
     <div
       id="footer2"
@@ -46,6 +45,11 @@ const Footer2 = () => {
         color: "#7f9cad",
         textAlign: "end",
         lineHeight: "1.1rem",
+        width: '72%',
+        margin: 'auto',
+        marginBottom: '40px',
+        paddingTop: '40px'
+
       }}
     >
       <div id="leftPartFooter2" style={{ textAlign: "start" }}>
@@ -66,24 +70,24 @@ const Footer2 = () => {
 
       {/* rightPart */}
       <div id="rightPartFooter2">
-        <div>
+        <div style={{ display: 'flex' }}>
           <SpanElem>
-            <AiFillFacebook />
+            <a href="https://www.linkedin.com/in/md-arshad-khan-350206154/"><AiFillLinkedin /></a>
           </SpanElem>
           <SpanElem>
-            <AiFillYoutube />
+            <a href="https://www.instagram.com/_mad___boxer_/"><AiFillInstagram /></a>
           </SpanElem>
           <SpanElem>
-            <AiFillLinkedin />
+            <a href="https://www.facebook.com/people/Arshad-Khan/100004164048208/"><AiFillFacebook /></a>
           </SpanElem>
           <SpanElem>
-            <AiFillInstagram />
+            <a href="https://www.youtube.com/channel/UCGK-satop1PZ9njUOYCzHJw"><AiFillYoutube /></a>
           </SpanElem>
           <SpanElem>
             <AiFillTwitterCircle />
           </SpanElem>
         </div>
-        <P> +1-855-738-8741</P>
+        <P> +91-8431290899</P>
         <P style={{ fontSize: "10px" }}>ESPAÑOL</P>
       </div>
     </div>
