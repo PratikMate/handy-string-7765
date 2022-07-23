e01d9e0ad8b7d437f7bb2ee98c853f615e65926e
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-23 18:17:29 +0530

    CSS Updated

--- client/src/components/HomePage/MidPart/Part4.jsx
+++ client/src/components/HomePage/MidPart/Part4.jsx
@@ -1,46 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-const Part4 = () => {
-  const WrapperPart = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    width: 90vw;
-    height: 75vh;
-    margin: auto;
-    color: #546e7a;
+const WrapperPart = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: auto;
+  color: #546e7a;
+  overflow: hidden;
+  margin-top:100px;
+`;
 
-    // border: 1px solid red;
-    overflow: hidden;
-  `;
-  const LeftList4Heading = styled.p`
+const LeftSideDiv = styled.div`
+  p:nth-child(2n + 1) {
+    margin-bottom: 1.2rem;
+    text-align: left;
+    font-size: 14px;
+  }
+  p:nth-child(2n):hover {
+    color: #1181b6;
+    padding:10px 0px;
+  }
+  p:nth-child(2n) {
     font-weight: bold;
     color: #2e393e;
+    padding:10px 0px;
     cursor: pointer;
-  `;
-  const LeftSideDiv = styled.div`
-    p:nth-child(2n + 1) {
-      // border: 1px solid red;
-      margin-bottom: 3rem;
-      text-align: left;
-      line-height: 2px;
-      font-size: 13px;
-    }
-    p:nth-child(2n):hover {
-      color: #1181b6;
-    }
-    p:nth-child(2n) {
-      font-weight: bold;
-      color: #2e393e;
-      cursor: pointer;
-      text-align: left;
-      line-height: 2px;
-    }
-  `;
+    text-align: left;
+    line-height: 2px;
+    font-size: 16px;
+  }
+`;
+const Part4 = () => {
   return (
     <WrapperPart>
-      <LeftSideDiv id="leftList4">
-        <LeftList4Heading>TIMEKEEPING</LeftList4Heading>
+      <LeftSideDiv >
+        <p style={{ color: '#78909c', cursor: 'pointer',fontWeight:'bold' ,fontSize: '16px', wordSpacing: '2px' }}>TIMEKEEPING</p>
         <p>Timer</p>
         <p>Track work hours in real time.</p>
         <p>Timesheet</p>
@@ -52,15 +46,13 @@ const Part4 = () => {
         <p>Kiosk</p>
         <p>Clock in from a shared device.</p>
       </LeftSideDiv>
-      <div id="rightList4">
+      <div >
         <img
-          src="https://clockify.me/assets/images/feature-time-tracker.svg"
+          src="https://clockify.me/assets/images/feature-time-tracker-methods.svg"
           alt="statcs pic"
-          width={"100%"}
         />
       </div>
     </WrapperPart>
   );
 };
-
 export default Part4;
