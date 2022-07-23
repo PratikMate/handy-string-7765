13714b512bb5cef9d55c8ffbf387400df37f4403
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-23 18:17:45 +0530

    CSS Updated

--- client/src/components/HomePage/MidPart/Part6.jsx
+++ client/src/components/HomePage/MidPart/Part6.jsx
@@ -1,79 +1,77 @@
 import React from "react";
-// import "./part4.css";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const WrapperPart = styled.div`
+display: flex;
+justify-content: space-evenly;
+margin: auto;
+color: #546e7a;
+overflow: hidden;
+margin-top:100px;
+`;
+
+const LeftSideDiv = styled.div`
+margin-top:50px;
+p:nth-child(2n + 1) {
+  margin-bottom: 1.2rem;
+  text-align: left;
+  font-size: 14px;
+}
+p:nth-child(2n):hover {
+  color: #1181b6;
+  padding:10px 0px;
+}
+p:nth-child(2n) {
+  font-weight: bold;
+  color: #2e393e;
+  padding:10px 0px;
+  cursor: pointer;
+  text-align: left;
+  line-height: 2px;
+  font-size: 16px;
+}
+`;
+
+const Button = styled.button`
+  color: white;
+  background: #0288d1;
+  padding: 20px 50px;
+  border: none;
+  margin-top:100px;
+  margin-bottom:50px;
+  border-radius: 5px;
+  font-size: 18px;
+  &:hover {
+    background: #03669b;
+  }
+`;
 const Part6 = () => {
-  const WrapperPart = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    width: 90vw;
-    height: 75vh;
-    margin: auto;
-    color: #546e7a;
-    overflow: hidden;
-  `;
-  const LeftList4Heading = styled.p`
-    font-weight: bold;
-    color: #2e393e;
-    cursor: pointer;
-  `;
-
-  const LeftSideDiv = styled.div`
-    width: 40%;
-    p:nth-child(2n + 1) {
-      margin-bottom: 3rem;
-      text-align: left;
-      // line-height: 0.1rem;
-      font-size: 13px;
-    }
-    p:nth-child(2n):hover {
-      color: #1181b6;
-    }
-    p:nth-child(2n) {
-      font-weight: bold;
-      color: #2e393e;
-      cursor: pointer;
-      text-align: left;
-      line-height: 0px;
-    }
-  `;
-
-  const Button = styled.button`
-    color: white;
-    background: #0288d1;
-    padding: 20px 50px;
-    border: none;
-    border-radius: 5px;
-    font-size: 18px;
-    &:hover {
-      background: #03669b;
-    }
-  `;
 
   return (
     <div>
       <WrapperPart>
-        <LeftSideDiv>
-          <LeftList4Heading>TIMEKEEPING</LeftList4Heading>
-          <p>Timer</p>
-          <p>Track work hours in real time.</p>
-          <p>Timesheet</p>
-          <p>Enter time in a weekly timesheet.</p>
-          <p>Calendar</p>
-          <p>Visually block out and manage time.</p>
-          <p>Auto tracker</p>
-          <p>Track apps and websites you use.</p>
-          <p>Kiosk</p>
-          <p>Clock in from a shared device.</p>
-        </LeftSideDiv>
-        <div id="rightList4">
+      <LeftSideDiv>
+        <p style={{ color: '#78909c', cursor: 'pointer',fontWeight:'bold' ,fontSize: '16px', wordSpacing: '2px' }}>MANAGEMENT</p>
+        <p>Scheduling</p>
+        <p>Schedule work, assignments, and shifts.</p>
+        <p>Time off</p>
+        <p>Manage leaves and holidays.</p>
+        <p>Approval</p>
+        <p>Submit and approve timesheets.</p>
+        <p>Invoicing</p>
+        <p>Create invoices from billable time.</p>
+        <p>Expenses</p>
+        <p>Record project expenses and fees.</p>
+      </LeftSideDiv>
+        <div >
           <img
-            src="https://clockify.me/assets/images/feature-team-activity.svg"
+            src="https://clockify.me/assets/images/feature-team-scheduling.svg"
             alt="statcs pic"
-            width={"100%"}
           />
         </div>
       </WrapperPart>
-      <Button>SEE ALL FEATURES</Button>
+      <Link to='/features'><Button>SEE ALL FEATURES</Button></Link>
     </div>
   );
 };
