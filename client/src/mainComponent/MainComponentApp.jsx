21888baf3ce5c08d26e666804498f58061bb92c6
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 12:12:15 +0530

    App Updated

--- client/src/mainComponent/MainComponentApp.jsx
+++ client/src/mainComponent/MainComponentApp.jsx
@@ -11,6 +11,7 @@ import Team from './SubComponents/Team'
 import Clients from './SubComponents/Clients'
 import Tags from './SubComponents/Tags'
 import Setting from './SubComponents/Setting'
+import Error from '../components/Error'
 
 const MainComponentApp = () => {
   const [toggle, setToggle] = useState(true)
@@ -27,7 +28,7 @@ const MainComponentApp = () => {
             </div>
             <div style={{background:'#f2f6f8', width:'100%', marginTop:'50px',height:'100vh'}}>
               <Routes>
-                <Route path='/timetracker' element={<TimeTracker/>} />
+                <Route path='/' element={<TimeTracker/>} />
                 <Route path='/calendar' element={<Calendar/>} />
                 <Route path='/dashboard' element={<Dashboard/>} />
                 <Route path='/reports' element={<Reports/>} />
@@ -36,6 +37,7 @@ const MainComponentApp = () => {
                 <Route path='/clients' element={<TimeTracker/>} />
                 <Route path='/tags' element={<Tags/>} />
                 <Route path='/setting' element={<Setting/>} />
+                <Route path='*' element={<Error/>} />
               </Routes>
             </div>
         </div>
