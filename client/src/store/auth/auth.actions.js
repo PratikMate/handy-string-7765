4ef3dd91ada5d87382f41e3e9ad0a5ec65fc7cc9
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 12:12:44 +0530

    Actions Updated

--- client/src/store/auth/auth.actions.js
+++ client/src/store/auth/auth.actions.js
@@ -1,42 +1,45 @@
 import axios from "axios";
 import { LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS, LOGOUT, SIGNUP_ERROR, SIGNUP_LOADING, SIGNUP_SUCCESS } from "./auth.types";
+import { useNavigate } from "react-router-dom";
 
 // form
-   // input feilds => api
-     // -username/email
-     // -password
+// input feilds => api
+// -username/email
+// -password
 
 // thunk will help us thus writing two functions
-export const loginAPI = (data) => (dispatch) => { 
+export const loginAPI = (data) => (dispatch) => {
+    const navigate = useNavigate()
     dispatch({ type: LOGIN_LOADING });
     axios
         .post("http://localhost:8080/api/auth/", {
-          "email": data.email,
-          "password": data.password
+            "email": data.email,
+            "password": data.password
         })
         .then((r) => {
             dispatch({ type: LOGIN_SUCCESS, payload: r.data });
             localStorage.setItem("token", JSON.stringify(r.data.data));
             localStorage.setItem("userData", JSON.stringify(r.data));
+
         })
         .catch(() => {
-        dispatch({ type: LOGIN_ERROR });
-    })
+            dispatch({ type: LOGIN_ERROR });
+        })
 }
-export const signupAPI = (data) => (dispatch) => { 
+export const signupAPI = (data) => (dispatch) => {
     console.log('data:', data)
     dispatch({ type: SIGNUP_LOADING });
     axios
         .post("http://localhost:8080/api/users/signup", {
-          "email": data.email,
-          "password": data.password
+            "email": data.email,
+            "password": data.password
         })
         .then((r) => {
-        dispatch({ type: SIGNUP_SUCCESS, payload:r.data });
+            dispatch({ type: SIGNUP_SUCCESS, payload: r.data });
         })
         .catch(() => {
-        dispatch({ type: SIGNUP_ERROR });
-    })
+            dispatch({ type: SIGNUP_ERROR });
+        })
 }
 
 export const logout = () => (dispatch) => {
