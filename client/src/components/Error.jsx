0d4a1a4b716368a6d6f436b8f4e84278c4954d17
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-07-24 12:11:43 +0530

    Error Page Created

---
+++ client/src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+        <img src='https://gifimage.net/wp-content/uploads/2017/09/404-gif-1.gif' style={{height:'90vh', margin:'auto'}} alt='error' />
+    </div>
+  )
+}
+
+export default Error
